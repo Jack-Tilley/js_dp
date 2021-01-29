42e167634c4d5132d70bb3c38d9b7e4e17a37730
Jack-Tilley <tilley.e.jack@gmail.com>
2021-01-29 13:36:57 -0800

    added howSum dp

---
+++ howSum.js
@@ -0,0 +1,24 @@
+// takes in targetSum and nums
+// returns an array containing any comb of eles that add up to target sum
+// if none return null
+// only return one
+
+const howSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+  for (let num of numbers) {
+    const res = howSum(targetSum - num, numbers, memo);
+    if (res !== null) {
+      memo[targetSum] = [...res, num];
+      return memo[targetSum];
+    }
+  }
+  memo[targetSum] = null;
+  return null;
+};
+
+console.log(howSum(7, [5, 4, 3, 7]));
+console.log(howSum(8, [5, 4, 3, 7]));
+console.log(howSum(2, [5, 4, 3, 7]));
+console.log(howSum(300, [7, 14]));
