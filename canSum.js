b56ab6e80a31a05da1f636acecb206cf1eaf6dec
Jack-Tilley <tilley.e.jack@gmail.com>
2021-01-29 13:18:59 -0800

    optimized canSum

--- canSum.js
+++ canSum.js
@@ -3,15 +3,23 @@
 // generate targetSum from nums in array
 // elements can be used more than once
 
-const canSum = (targetSum, arr) => {
+const canSum = (targetSum, arr, memo = {}) => {
+  if (targetSum in memo) {
+    return memo[targetSum];
+  }
   if (targetSum === 0) return true;
   if (targetSum < 0) return false;
   for (let e of arr) {
-    if (canSum(targetSum - e, arr) === true) return true;
+    if (canSum(targetSum - e, arr, memo) === true) {
+      memo[targetSum] = true;
+      return true;
+    }
   }
+  memo[targetSum] = false;
   return false;
 };
 
 console.log(canSum(7, [5, 4, 3, 7]));
 console.log(canSum(8, [5, 4, 3, 7]));
 console.log(canSum(2, [5, 4, 3, 7]));
+console.log(canSum(300, [7, 14]));
